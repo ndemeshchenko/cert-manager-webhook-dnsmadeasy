9a90f335ec51d4307e4df08515108e8ec99b7451
Mykyta Demeshchenko <mykyta@textkernel.nl>
2022-06-23 11:40:58 +0200

    upd
    Signed-off-by: Mykyta Demeshchenko <mykyta@textkernel.nl>

--- main.go
+++ main.go
@@ -294,6 +294,7 @@ func loadConfig(cfgJSON *extAPI.JSON) (dnsmadeasyDNSProviderConfig, error) {
 	return cfg, nil
 }
 
+// printError function here
 func printError(err error) {
 	klog.Errorf("\n\nERROR\n %v \n\n", err)
 }
