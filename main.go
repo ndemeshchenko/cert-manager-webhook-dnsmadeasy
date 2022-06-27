99bc2abbc50299e63bb6405ffb3891e5e62c35f0
Mykyta Demeshchenko <mykyta@textkernel.nl>
2022-06-27 11:53:31 +0200

    update comment main.go

--- main.go
+++ main.go
@@ -294,7 +294,7 @@ func loadConfig(cfgJSON *extAPI.JSON) (dnsmadeasyDNSProviderConfig, error) {
 	return cfg, nil
 }
 
-// printError func
+// printError
 func printError(err error) {
 	klog.Errorf("\n\nERROR\n %v \n\n", err)
 }
